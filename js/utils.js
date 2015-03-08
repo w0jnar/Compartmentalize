62ee34db04064451ad906cda9f13245e14a9f600
Thomas Wojnar <wojnarthomas@gmail.com>
2015-03-08 04:48:28 -0400

    More baseline code. Have the start of module code.
    Do not have the code for the general module framework yet, or the more
    specific code yet.

---
+++ js/utils.js
@@ -0,0 +1,30 @@
+﻿var supportedModules = [
+    {
+        name: "Slack",
+        functionPointer: "generateSlackModule",
+        id: "slackId"
+    }
+];
+
+function generateList() {
+    var listElement;
+    for (var i = 0; i < supportedModules.length; i++) {
+        listElement = "<li><a href=\"javascript:void(0);\"  onclick=\"javascript:checkCheckBox('" + supportedModules[i].id + "');\">" + supportedModules[i].name + "<input type=\"checkbox\" id=\"" + supportedModules[i].id + "\" style=\"float:right;\"/></a></li>";
+        // listElement = "<li><a href=\"javascript:void(0);\" onclick=\"javascript:console.log('meow');\">" + supportedModules[i].name + "<input type=\"checkbox\" id=\"" + supportedModules[i].id + "\" onclick=\"javascript: console.log('meow');\" style=\"float:right;\"/></a></li>";
+        $("#selectionDropdown").append(listElement);
+    }
+};
+
+function checkCheckBox(idName) {
+    if (event.target.nodeName !== "INPUT") {
+        $("#" + idName).prop('checked', !$("#" + idName).is(':checked'));
+    }
+}
+
+function generateModules() {
+    for (var i = 0; i < supportedModules.length; i++) {
+        if ($("#" + supportedModules[i].id).is(':checked')) {
+            console.log("meow");
+        }
+    }
+};
